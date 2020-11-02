fe1d4844a1a67b40718c0ddbfc4be1c005e26ebe
avinash bandi <73810309+bandiav1@users.noreply.github.com>
2020-11-02 21:44:21 +0530

    Delete complete.R

--- complete.R
+++
@@ -1,9 +0,0 @@
-complete = function(directory, id = 1:332) {
-    files = list.files(paste('C:/Users/bandiav1/Desktop/datasciencecoursera/', directory, sep = ''), pattern = '.csv', full.names = T)
-    temp1 <<- data.frame(id = numeric(), nobs = numeric())
-    for (i in id) {
-      temp = read.csv(files[i])[, c('nitrate', 'sulfate')]
-      temp1 <<- rbind(temp1, data.frame(id = i, nobs = sum(complete.cases(temp))))
-    }
-    temp1
-}
